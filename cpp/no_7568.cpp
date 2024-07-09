8714ed698660817807382c40a68563b32f2e70fc
meongju0o0 <bl4angel20@khu.ac.kr>
2024-07-10 02:18:48 +0900

    .

--- cpp/no_7568.cpp
+++ cpp/no_7568.cpp
@@ -1,22 +1,8 @@
 ﻿#include <iostream>
 #include <tuple>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
-bool build_cmp (tuple<int, int, int, int>& a, tuple<int, int, int, int> b) {
-    if(get<1>(a) < get<1>(b) && get<2>(a) < get<2>(b)) {
-        return false;
-    }
-    else {
-        return get<0>(a) > get<0>(b);
-    }
-}
-
-bool idx_cmp (tuple<int, int, int, int>& a, tuple<int, int, int, int> b) {
-    return get<0>(a) < get<0>(b);
-}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -25,43 +11,25 @@ int main() {
     int n;
     cin >> n;
 
-    vector<tuple<int, int, int, int>> table; //(인덱스 id, 몸무게, 키, 순위)
+    vector<tuple<int, int, int>> table; //(몸무게, 키, 순위)
     for(int i = 0; i < n; i++) {
         int weight, height;
         cin >> weight >> height;
-        table.emplace_back(i, weight, height, 0);
+        table.emplace_back(weight, height, 0);
     }
 
-    sort(table.begin(), table.end(), build_cmp);
-
-    int prev_weight, prev_height, cur_weight, cur_height;
-    int idx = 0, rank = 1;
-    for(tuple<int, int, int, int>& record: table) {
-        if(idx == 0) {
-            cur_weight = get<1>(record);
-            cur_height = get<2>(record);
-            get<3>(record) = rank;
-        }
-        else {
-            prev_weight = cur_weight;
-            prev_height = cur_height;
-            cur_weight = get<1>(record);
-            cur_height = get<2>(record);
-            if(prev_weight > cur_weight && prev_height > cur_height) {
-                rank = idx + 1;
-                get<3>(record) = rank;
-            }
-            else {
-                get<3>(record) = rank;
+    for(tuple<int, int, int>& record: table) {
+        int rank = 1;
+        for(tuple<int, int, int> temp_record: table) {
+            if(get<0>(record) < get<0>(temp_record) && get<1>(record) < get<1>(temp_record)) {
+                rank++;
             }
         }
-        idx++;
+        get<2>(record) = rank;
     }
 
-    sort(table.begin(), table.end(), idx_cmp);
-
-    for(tuple<int, int, int, int>& record: table) {
-        printf("%d ", get<3>(record));
+    for(tuple<int, int, int>& record: table) {
+        printf("%d ", get<2>(record));
     }
 
     return 0;
