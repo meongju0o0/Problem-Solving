70deb538c91dad56deaf53e7a445b977f64a4aa5
meongju0o0 <bl4angel20@khu.ac.kr>
2024-07-27 22:21:40 +0900

    .

--- README.md
+++ README.md
@@ -53,6 +53,7 @@
 - [no_24444(pure DFS)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_24444.cpp)
 - [no_24445(pure BFS)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_24445.cpp)
 - [no_24480(pure BFS)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_24480.cpp)
+- [no_2606(BFS)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_2606.cpp)
 
 ### Dijkstra
 - [no_1753(shortest path's weight)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_1753.cpp)
---
+++ cpp/no_2606.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+class Graph {
+private:
+    vector<vector<int>> edges;
+public:
+    Graph() = default;
+
+    explicit Graph(const vector<vector<int>>& _edges) {
+        edges = _edges;
+    }
+
+    [[nodiscard]] vector<int> get_neighbors(const int& vertex) {
+        return edges[vertex];
+    }
+};
+
+int num_infected_computer(Graph& graph) {
+    unordered_set<int> visited;
+    queue<int> candidates;
+    candidates.push(0);
+
+    while(!candidates.empty()) {
+        int cur_vertex = candidates.front();
+        candidates.pop();
+        if(!visited.contains(cur_vertex)) {
+            visited.insert(cur_vertex);
+            for(const int& neighbor: graph.get_neighbors(cur_vertex)) {
+                if(!visited.contains(neighbor)) {
+                    candidates.push(neighbor);
+                }
+            }
+        }
+    }
+    return static_cast<int>(visited.size()) - 1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int num_vertices, num_edges;
+    cin >> num_vertices >> num_edges;
+
+    vector<vector<int>> edges(num_vertices);
+
+    for(int i = 0; i < num_edges; i++) {
+        int u, v;
+        cin >> u >> v;
+        edges[u-1].push_back(v-1);
+        edges[v-1].push_back(u-1);
+    }
+
+    Graph graph = Graph(edges);
+
+    printf("%d", num_infected_computer(graph));
+
+    return 0;
+}
