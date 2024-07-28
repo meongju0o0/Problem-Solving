dd81e5408bf50898ac8b344b99730cb6b148465a
meongju0o0 <bl4angel20@khu.ac.kr>
2024-07-29 02:03:10 +0900

    .'

--- README.md
+++ README.md
@@ -140,7 +140,7 @@
 - [no_11053(LIS, Longest Increasing Subsequence)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_11053.cpp)
 - [no_11722(LDS, Longest Decreasing Subsequence)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_11722.cpp)
 - [no_11054(LBS, Longest Bitonic Subsequence)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_11054.cpp)
-
+- [no_2579(Going up the stairs)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_2579.cpp)
 
 ## Accumulated Sum
 - [no_11659(accumulated sum)](https://github.com/meongju0o0/baekjoon/blob/master/cpp/no_11659.cpp)
---
+++ cpp/no_2579.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> scores(n);
+    for (int &score : scores) {
+        cin >> score;
+    }
+
+    vector<int> accumulated_scores(n);
+    if (n >= 1) {
+        accumulated_scores[0] = scores[0];
+    }
+    if (n >= 2) {
+        accumulated_scores[1] = scores[0] + scores[1];
+    }
+    if (n >= 3) {
+        accumulated_scores[2] = max(scores[0] + scores[2], scores[1] + scores[2]);
+    }
+
+    for (int i = 3; i < n; i++) {
+        accumulated_scores[i] = max(accumulated_scores[i - 2] + scores[i],
+                                    accumulated_scores[i - 3] + scores[i - 1] + scores[i]);
+    }
+
+    cout << accumulated_scores[n - 1] << endl;
+
+    return 0;
+}
