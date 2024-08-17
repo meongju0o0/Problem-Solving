ae48b01030a9da8ae1c792d7b201769cb5e7343c
meongju0o0 <bl4angel20@khu.ac.kr>
2024-08-18 01:50:27 +0900

    .

--- cpp/no_10974.cpp
+++ cpp/no_10974.cpp
@@ -1,20 +1,19 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
-#include <unordered_set>
 
 using namespace std;
 
-void permutation(vector<string>& permu, unordered_set<int>& visited, const string& cur_permu, const int n) {
+void permutation(vector<bool>& visited, const string& cur_permu, const int n) {
     if(cur_permu.size() == 2 * n) {
         printf("%s\n", cur_permu.c_str());
     }
     else {
         for(int i = 1; i <= n; i++) {
-            if(!visited.contains(i)) {
-                visited.insert(i);
-                permutation(permu, visited, cur_permu + to_string(i) + " ", n);
-                visited.erase(i);
+            if(!visited[i]) {
+                visited[i] = true;
+                permutation(visited, cur_permu + to_string(i) + " ", n);
+                visited[i] = false;
             }
         }
     }
@@ -27,9 +26,8 @@ int main() {
     int n;
     cin >> n;
 
-    vector<string> permu;
-    unordered_set<int> visited;
-    permutation(permu, visited, "", n);
+    vector<bool> visited(n, false);
+    permutation(visited, "", n);
 
     return 0;
 }
