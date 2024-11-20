203c037e6ec962de65885a7e05c4b4f679cf8ef9
meongju0o0 <bl4angel20@khu.ac.kr>
2024-11-20 18:24:54 +0900

    .

---
+++ cpp/BOJ5582.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    const size_t str1_len = str1.size();
+    const size_t str2_len = str2.size();
+
+    vector<vector<int>> memo(str1_len + 1, vector<int>(str2_len + 1, 0));
+    int max_len = 0;
+
+    for(size_t i = 1; i <= str1_len; i++) {
+        for(size_t j = 1; j <= str2_len; j++) {
+            if(str1[i - 1] == str2[j - 1]) {
+                memo[i][j] = memo[i - 1][j - 1] + 1;
+                max_len = max(max_len, memo[i][j]);
+            }
+        }
+    }
+
+    printf("%d", max_len);
+
+    return 0;
+}
---
+++ cpp/BOJ9655.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void calculate_subtree_sizes(const vector<vector<int>>& adj, vector<int>& subtree_sizes, int cur, int parent) {
+    subtree_sizes[cur] = 1;
+    for(const int neighbor : adj[cur]) {
+        if(neighbor != parent) {
+            calculate_subtree_sizes(adj, subtree_sizes, neighbor, cur);
+            subtree_sizes[cur] += subtree_sizes[neighbor];
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, r, q;
+    cin >> n >> r >> q;
+
+    vector<vector<int>> adj(n);
+    for(int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u - 1].push_back(v - 1);
+        adj[v - 1].push_back(u - 1);
+    }
+
+    vector<int> subtree_sizes(n, 0);
+
+    calculate_subtree_sizes(adj, subtree_sizes, r - 1, -1);
+
+    for(int i = 0; i < q; i++) {
+        int u;
+        cin >> u;
+        printf("%d\n", subtree_sizes[u - 1]);
+    }
+
+    return 0;
+}
